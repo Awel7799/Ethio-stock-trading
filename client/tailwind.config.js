1cc26170b9275c7417b522632f187d4c1ba0eb29
Awel7799 <awelkassim45@gmail.com>
2025-07-11 23:36:55 -0700

    tailwind package added and configured

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // All components inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
