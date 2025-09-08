911be810d53c937e8324f236d567ee0be3209154
Aschalew Daraje <aschalewdere2@gmail.com>
2025-09-08 01:12:47 -0700

    Update stockRoutes.js

--- services/routes/stockRoutes.js
+++ services/routes/stockRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTopGainersLive } = require('../controllers/stockController');
+const { getTopGainersLive } = require('../Controllers/stockController')
 
 // GET /api/stocks/gainers?limit=5
 router.get('/gainers', getTopGainersLive);
