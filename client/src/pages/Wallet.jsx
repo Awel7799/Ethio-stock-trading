3705b1880d16db1cfa882c5399e4faaa650bb83f
Awel7799 <awelkassim45@gmail.com>
2025-09-16 11:39:10 -0700

    fix delpoy

--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -1,5 +1,5 @@
 //pages/Wallet.jsx
-import Wallett from "../components/wallet/Wallet"
+import Wallett from "../components/wallet/wallet.jsx"
 export default function Wallet() {
   return (
     <div className="container mx-auto px-4 py-8">
