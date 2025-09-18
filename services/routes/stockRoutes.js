10cc06ec5ef39cb396a6d254c356651ec917db20
Aschalew Daraje <aschalewdere2@gmail.com>
2025-09-17 19:12:06 -0700

    Update stockRoutes.js

--- services/routes/stockRoutes.js
+++ services/routes/stockRoutes.js
@@ -1,7 +1,17 @@
+// routes/stockRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getStockDetails } = require('../Controllers/stockDetailController');
+const { getStockDetails } = require('../controllers/stockDetailController');
+const { getTopGainersLive } = require('../controllers/stockController');
 
+// Existing route for stock details
 router.get('/details/:symbol', getStockDetails);
+
+// NEW ROUTE - This was missing and causing 404 for gainers
+router.get('/gainers', getTopGainersLive);
+
+// If you have other stock-related routes, add them here
+// router.get('/search/:query', searchStocks);
+// router.get('/quote/:symbol', getStockQuote);
 
 module.exports = router;
