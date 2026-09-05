e2d14853a04981ca6ade9a91deb6886aa0df1feb
Awel7799 <awelkassim45@gmail.com>
2026-09-05 07:11:37 -0700

    fixed auth issue

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,6 @@
 const configuredBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+const normalizedBaseUrl = configuredBaseUrl.replace(/\/$/, "");
 
-export const API_BASE_URL = configuredBaseUrl.replace(/\/$/, "");
+export const API_BASE_URL = normalizedBaseUrl.endsWith("/api")
+	? normalizedBaseUrl
+	: `${normalizedBaseUrl}/api`;
